076f72241330622ed71c6a50552903f3ab626329
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2022-04-05 22:30:25 -0300

    Don't remove public dir
    Although this is the only way of making sure no stale files are kept. Sigh.

--- make-site.sh
+++ make-site.sh
@@ -12,6 +12,5 @@ fi
 # manually to the load-path; this will break as soon as it is updated
 # and the directory name changes, but alas I seldom update this blog
 # and it is not cost-effective for me to find a better solution now :)
-rm -r public/
-mkdir public/
+mkdir -p public/
 emacs --batch --no-init-file --no-site-lisp --directory "~/.emacs.d/elpa/htmlize-20210825.2150/" --load "$SCRIPTDIR/publish.el" --eval "(org-publish-all $FORCE)"
