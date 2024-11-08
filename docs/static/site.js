ea0ccb9d6d33553e1cf2250037536f8eaa109220
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2024-11-08 17:19:01 -0300

    Automated commit (this branch is for build artifacts only)

--- docs/static/site.js
+++ docs/static/site.js
@@ -1,6 +1,10 @@
 
 { /* Set background */
-    const possibleBackgrounds = [{background: "/static/dubabin-60.svg", size: "5%"}, {background: "/static/dubabin-75.svg", size: "2.5%"}];
+  const possibleBackgrounds = [
+    {background: "/static/dubabin-60.svg", size: "5%"},
+    {background: "/static/dubabin-75.svg", size: "2.5%"},
+    {background: "/static/dubabin-298-A.svg", size: "5%"},
+  ];
     const daysSinceEpoch = Math.floor(Date.now() / 86400000);
     // pick background based on day, so that it is stable (doesn't
     // change at every page reload), but still changes reliably
